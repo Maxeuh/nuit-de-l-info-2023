3c876c6d02bdc030175e7504e1e22584076158e5
Tructruc <emilien.fieu@etu.univ-tlse2.fr>
2023-12-08 06:55:03 +0100

    jeu

--- questionnaire/assets/js/questionnaire.js
+++ questionnaire/assets/js/questionnaire.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
         var continueButton = document.createElement('button');
         continueButton.innerText = 'Continuer';
         continueButton.classList.add('btn', 'btn-light', 'w-100', 'mb-3');
-        continueButton.setAttribute('onclick', 'document.getElementById("iframe_jeu").src = "/back/questionnaire/JeuQuestions/questions.html"');
+        continueButton.setAttribute('onclick', 'document.getElementById(\"iframe_jeu\").src = \"/back/questionnaire/JeuQuestions/questions.html\"');
         answersContainer.appendChild(continueButton);
 
         if (score < 0) {
