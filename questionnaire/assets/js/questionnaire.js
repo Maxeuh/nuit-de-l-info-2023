28f2a3b1082cbc54a94aaab2e300321778d5da4d
Tructruc <emilien.fieu@etu.univ-tlse2.fr>
2023-12-08 06:44:59 +0100

    jeu

--- questionnaire/assets/js/questionnaire.js
+++ questionnaire/assets/js/questionnaire.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
         var continueButton = document.createElement('button');
         continueButton.innerText = 'Continuer';
         continueButton.classList.add('btn', 'btn-light', 'w-100', 'mb-3');
-        continueButton.setAttribute('onclick', 'console.log("nextGame"); nextGame');
+        continueButton.setAttribute('onclick', 'nextGame');
         answersContainer.appendChild(continueButton);
 
         if (score < 0) {
